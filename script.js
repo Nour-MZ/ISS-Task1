d381c6deb9225e581ec8f6aaf473333ad4f52289
Nour-MZ <77830568+Nour-MZ@users.noreply.github.com>
2024-05-27 11:24:04 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,60 +1,63 @@
-
-
-const imagerx = document.querySelector('.imagerx');
-const imagery = document.querySelector('.imagery');
-const awardsection = document.querySelector('#awardsection');
-
-
-document.addEventListener('DOMContentLoaded', function() {
-  const menu = document.getElementById('ne-menu');
-  const menuToggle = document.getElementById('ne-menu-toggle');
-  const menuClose = document.getElementById('ne-menu-close');
-
-  menuToggle.addEventListener('click', function() {
-      menu.classList.add('active');
+const image1observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      imagerx.classList.add('imgoo-animation');
+    }
   });
+});
 
-  menuClose.addEventListener('click', function() {
-      menu.classList.remove('active');
+const image2observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      imagery.classList.add('imgoo-animation');
+    }
   });
 });
 
 
-const image1observer = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-      if (entry.isIntersecting) {
-        imagerx.classList.add('imgoo-animation');
-      }
-    });
+const Section1observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('visible');
+
+
+    }
   });
+});
 
-  const image2observer = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-      if (entry.isIntersecting) {
-        imagery.classList.add('imgoo-animation');
-      }
-    });
+const numberobserver = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      increment("customers", 200);
+      increment("projects", 285);
+      increment("awards", 42);
+    }
   });
+});
 
-  const numberobserver = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-      if (entry.isIntersecting) {
-        increment("customers", 200);
-        increment("projects", 285);
-        increment("awards", 42);
-      }
-    });
-  });
+const observerOptions = {
+  root: null,
+  rootMargin: '0px',
+  threshold: 0.1
+};
 
-  image1observer.observe(imagerx);
-  image2observer.observe(imagery);
+const observer = new IntersectionObserver((entries, observer) => {
+entries.forEach(entry => {
+    if (entry.isIntersecting) {
+        entry.target.classList.add('visible');
+        observer.unobserve(entry.target);
+        console.log("good")
+    }
+});
+}, observerOptions);
 
-  numberobserver.observe(awardsection);
 
 
-  
-  
-  function increment(elem, finalVal) {
+
+
+
+
+function increment(elem, finalVal) {
     var currVal = parseInt(document.getElementById(elem).innerHTML, 10);
     if (currVal < finalVal) {
       currVal++;
@@ -65,3 +68,49 @@ const image1observer = new IntersectionObserver(entries => {
       }, 40)
     }
   };
+
+
+
+  const menu = document.getElementById('ne-menu');
+  const menuToggle = document.getElementById('ne-menu-toggle');
+  const menuClose = document.getElementById('ne-menu-close');
+  const images = document.querySelectorAll('.img-overlay-wrapper');
+  const bigimage = document.querySelector(".big-scale")
+  const imagerx = document.querySelector('.imagerx');
+  const imagery = document.querySelector('.imagery');
+  const awardsection = document.querySelector('#awardsection');
+  const aboutsection = document.querySelector('.bg-secondo');
+  const emailimage = document.querySelector(".emailimage")
+  const partnersection = document.querySelector(".partnersection");
+
+
+  menuToggle.addEventListener('click', function() {
+      menu.classList.add('active');
+  });
+
+  menuClose.addEventListener('click', function() {
+      menu.classList.remove('active');
+  });
+
+
+    observer.observe(partnersection)
+    observer.observe(emailimage);
+    observer.observe(bigimage);
+    Section1observer.observe(aboutsection);
+    image1observer.observe(imagerx);
+    image2observer.observe(imagery);
+    numberobserver.observe(awardsection);
+
+    images.forEach(elimage => {
+      observer.observe(elimage);
+      console.log("good")
+    });
+    
+
+
+
+
+
+  
+  
+  
